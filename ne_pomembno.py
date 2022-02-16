007826fd47afb42f72734ece5243fff19587e997
Tim Rekelj <rekeljtim@gmail.com>
2022-02-16 10:26:32 +0100

    Translated from SLO to ENG and updated README.md

--- ne_pomembno.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from app.models import Uporabniki, Kategorije
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Uporabniki': Uporabniki, 'Kategorije': Kategorije}
